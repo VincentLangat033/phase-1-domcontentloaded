0dc6fd69456a0b5219e5365fb7710bb5dbf69013
Vincent Langat <kimutailangat033@gmail.com>
2022-06-15 21:24:59 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+// Your code goes heredocument.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener('DOMContentLoaded', function(){
+        let newText = document.getElementById("text")
+        return newText.textContent = "This is really cool!";
+
+    })
